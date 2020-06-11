4bad89ab3fe1b6e94af1700908d56715b93c7385
GeeTransit <geetransit@gmail.com>
2020-06-11 00:04:24 -0400

    Add Utils class

---
+++ src/Utils.java
@@ -0,0 +1,25 @@
+/*
+ahbejarano
+2020-06-11
+Utility methods.
+*/
+
+package geetransit.minecraft05.engine;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+import java.nio.charset.StandardCharsets;
+
+public class Utils {
+
+	public static String loadResource(String fileName) throws Exception {
+		try (
+			InputStream in = Utils.class.getResourceAsStream(fileName);
+			Scanner scanner = new Scanner(in, StandardCharsets.UTF_8.name());
+		) {
+			return scanner.useDelimiter("\\A").next();
+		}
+	}
+
+}
