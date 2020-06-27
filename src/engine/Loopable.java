894edfc7eea9e79b38a6256ba5bdd821f4cddf7d
GeeTransit <geetransit@gmail.com>
2020-06-27 00:57:49 -0400

    Add default implementations to Loopable

--- src/engine/Loopable.java
+++ src/engine/Loopable.java
@@ -6,4 +6,15 @@ Loopable interface class. (used by Window)
 package geetransit.minecraft05.engine;
 
 public interface Loopable extends Initializable, Inputtable, Updateable, Renderable {
+	@Override
+	default void init(Window window) throws Exception {}
+
+	@Override
+	default void input(Window window) {}
+
+	@Override
+	default void update(float interval) {}
+
+	@Override
+	default void render(Window window) {}
 }
