84d846e91ebba2ec57521046b1790ebb516781e3
GeeTransit <geetransit@gmail.com>
2020-06-30 23:50:37 -0400

    Add glass block (transparent)
    New "groups" of block names (addGroup)
    Block item maps changed (map<mesh, ...> -> map<group, ...>)
    Merge addBlock,newBlock -> addBlock
    Add reordering of block items using distance from camera (reorderBlock)

--- src/game/World.java
+++ src/game/World.java
@@ -26,8 +26,10 @@ public class World implements Loopable {
 	private final Countdown countdown;
 
 	private Shader shader;
-	private final Map<String, Mesh> meshMap;
-	private final Map<Mesh, List<BlockItem>> blockMap;
+	private final Map<String, Mesh> meshMap;  // name -> mesh
+	private final Map<String, List<String>> groupMap;  // group -> list<name>
+	private final Map<String, Boolean> orderMap;  // group -> ordered
+	private final Map<String, List<BlockItem>> blockMap;  // group -> list<block>
 	private final List<BlockItem> blockList;
 
 	private final ClosestItem<BlockItem> closestItem;
@@ -40,6 +42,8 @@ public class World implements Loopable {
 		this.countdown = new Countdown(CHANGE_DELAY);
 
 		this.meshMap = new HashMap<>();
+		this.groupMap = new HashMap<>();
+		this.orderMap = new HashMap<>();
 		this.blockMap = new HashMap<>();
 		this.blockList = new ArrayList<>();
 
@@ -64,10 +68,15 @@ public class World implements Loopable {
 		this.shader.create("isTextured");
 		this.shader.create("isSelected");
 
-		// Create the blocks' mesh
-		this
-			.putMesh("grassblock", this.loadMesh("/res/cube-fblr,u,d.obj", "/res/grassblock.png"))
-			.putMesh("cobbleblock", this.loadMesh("/res/cube-fblrud.obj", "/res/cobbleblock.png"))
+		// create groups and block meshes
+		this.addGroup("grass", false);
+		this.putMesh("grass", "grassblock", this.loadMesh("/res/cube-fblr,u,d.obj", "/res/grassblock.png"));
+
+		this.addGroup("cobble", false);
+		this.putMesh("cobble", "cobbleblock", this.loadMesh("/res/cube-fblrud.obj", "/res/cobbleblock.png"));
+
+		this.addGroup("glass", true);
+		this.putMesh("glass", "glassblock", this.loadMesh("/res/cube-fblrud.obj", "/res/glassblock.png"));
 
 		// get heightmap
 		try (HeightMap map = HeightMap.loadFromImage("/res/heightmap.png")) {
@@ -75,19 +84,18 @@ public class World implements Loopable {
 			for (int x = 0; x < map.width; x++) {
 				for (int z = 0; z < map.length; z++) {
 					int y = (int) map.compressExpand(map.heightAt(x, z), 0, map.MAX_COLOR, 0, 16);
-					this.addBlock(this.newBlock("grassblock").setPosition(x, y, z));
+					this.addBlock("grassblock", x, y, z);
 					for (int k = y-1; k >= Math.max(y-2, 0); k--)
-						this.addBlock(this.newBlock("cobbleblock").setPosition(x, k, z));
+						this.addBlock("cobbleblock", x, k, z);
 				}
 			}
 		}
 
 		// add spawn markers (-2z is forwards)
-		this
-			.addBlock(this.newBlock("grassblock").setPosition(+1, +1,  0))
-			.addBlock(this.newBlock("grassblock").setPosition(-1, +1,  0))
-			.addBlock(this.newBlock("grassblock").setPosition( 0, +1, +1))
-			.addBlock(this.newBlock("grassblock").setPosition( 0, +1, -2));
+		this.addBlock("grassblock", +1, +1,  0);
+		this.addBlock("grassblock", -1, +1,  0);
+		this.addBlock("grassblock",  0, +1, +1);
+		this.addBlock("grassblock",  0, +1, -2);
 	}
 
 	@Override
@@ -112,6 +120,7 @@ public class World implements Loopable {
 		if (window.isKeyDown(GLFW_KEY_0)) this.change = "";
 		if (window.isKeyDown(GLFW_KEY_1)) this.change = "grassblock";
 		if (window.isKeyDown(GLFW_KEY_2)) this.change = "cobbleblock";
+		if (window.isKeyDown(GLFW_KEY_3)) this.change = "glassblock";
 		if (this.change == null) this.countdown.reset();
 	}
 
@@ -119,10 +128,14 @@ public class World implements Loopable {
 	public void update(float interval) {
 		// movement
 		this.camera.movePosition(this.movement, interval*MOVEMENT_STEP);
+		if (!this.movement.equals(0, 0, 0))
+			for (Map.Entry<String, Boolean> entry : this.orderMap.entrySet())
+				if (entry.getValue())
+					this.reorderBlock(entry.getKey());
 
-		// placing / removing
 		this.countdown.add(interval);
 		if (this.change != null && this.countdown.nextOnce()) {
+			// placing / removing
 			this.closestItem.update(this.blockList, this.camera);
 			if (this.closestItem.closest != null) {
 				if (this.change.equals("")) {
@@ -139,7 +152,7 @@ public class World implements Loopable {
 							if (block.getPosition().equals(position))
 								break check;
 						// else
-						this.addBlock(this.newBlock(this.change).setPosition(position));
+						this.addBlock(this.change, position);
 					}
 				}
 			}
@@ -161,25 +174,27 @@ public class World implements Loopable {
 		glEnable(GL_CULL_FACE);
 		glCullFace(GL_BACK);
 
-		// view
-		Matrix4f viewMatrix = this.camera.getViewMatrix();
-
 		// update visible blocks
 		for (BlockItem block : this.blockList)
 			block.setVisible(this.camera.insideFrustum(block.getPosition(), BLOCK_RADIUS*block.getScale()));
 
-		// draw blocks
-		Matrix4f temp = new Matrix4f();
-		for (Map.Entry<Mesh, List<BlockItem>> entry : this.blockMap.entrySet())
-			entry.getKey().render(
-				this.shader,
-				entry.getValue().stream().filter(item -> item.isVisible()),
-				(shader, item) -> {
-					item.buildModelViewMatrix(viewMatrix, temp);
-					shader.set("modelViewMatrix", temp);
-					shader.set("isSelected", item.isSelected());
-				}
-			);
+		// reorder ordered groups
+		for (String group : this.blockMap.keySet())
+			if (this.orderMap.get(group))
+				this.reorderBlock(group);
+
+		Matrix4f viewMatrix = this.camera.getViewMatrix();  // view matrix
+		Matrix4f temp = new Matrix4f();  // temporary matrix (stores model view matrix)
+
+		// opaque blocks
+		for (String group : this.groupMap.keySet())
+			if (!this.orderMap.get(group))
+				this.renderBlock(group, viewMatrix, temp);
+
+		// transparent blocks
+		for (String group : this.groupMap.keySet())
+			if (this.orderMap.get(group))
+				this.renderBlock(group, viewMatrix, temp);
 
 		glDisable(GL_CULL_FACE);
 		this.shader.unbind();
@@ -199,9 +214,27 @@ public class World implements Loopable {
 		return mesh;
 	}
 
-	private World putMesh(String name, Mesh mesh) {
+	private void putMesh(String group, String name, Mesh mesh) {
+		// mesh
+		if (this.meshMap.containsKey(name))
+			throw new RuntimeException("mesh already defined: "+name);
 		this.meshMap.put(name, mesh);
-		return this;
+
+		// group
+		if (!this.groupMap.containsKey(group))
+			throw new RuntimeException("group not defined: "+group);
+		if (!this.groupMap.get(group).contains(name))
+			this.groupMap.get(group).add(name);
+	}
+
+	private void addGroup(String group, boolean order) { this.addGroup(group, order, false); }
+	private void addGroup(String group, boolean order, boolean redefine) {
+		if (!redefine && this.groupMap.containsKey(group))
+			throw new RuntimeException("group already defined: "+group);
+
+		this.groupMap.put(group, new ArrayList<>());
+		this.orderMap.put(group, order);
+		this.blockMap.put(group, new ArrayList<>());
 	}
 
 	private BlockItem newBlock(String name) {
@@ -209,21 +242,68 @@ public class World implements Loopable {
 		return new BlockItem(mesh).setScale(BLOCK_SCALE);
 	}
 
-	private World addBlock(BlockItem block) {
-		Mesh mesh = block.getMesh();
-		this.blockList.add(block);
-		if (!this.blockMap.containsKey(mesh))
-			this.blockMap.put(mesh, new ArrayList<>());
-		this.blockMap.get(mesh).add(block);
-		return this;
+	private void renderBlock(String group, Matrix4f viewMatrix, Matrix4f temp) {
+		List<BlockItem> blocks = this.blockMap.get(group);
+		if (this.groupMap.get(group).size() == 1) {
+			if (blocks.size() > 0){
+				blocks.get(0).getMesh().render(
+					this.shader,
+					blocks.stream().filter(block -> block.isVisible()),
+					(shader, block) -> {
+						block.buildModelViewMatrix(viewMatrix, temp);
+						shader.set("modelViewMatrix", temp);
+						shader.set("isSelected", block.isSelected());
+					}
+				);
+			}
+		} else {
+			for (BlockItem block : blocks)
+				block.getMesh().render(this.shader, block, (shader, block2) -> {
+					block.buildModelViewMatrix(viewMatrix, temp);
+					shader.set("modelViewMatrix", temp);
+					shader.set("isSelected", block2.isSelected());
+				});
+		}
 	}
 
-	private World removeBlock(BlockItem block) {
-		Mesh mesh = block.getMesh();
+	private String nameOf(BlockItem block) {
+		for (Map.Entry<String, Mesh> entry : this.meshMap.entrySet())
+			if (entry.getValue() == block.getMesh())
+				return entry.getKey();
+		throw new RuntimeException("could not find parent name: "+block);
+	}
+	private String groupOf(String name) {
+		for (Map.Entry<String, List<String>> entry : this.groupMap.entrySet())
+			if (entry.getValue().contains(name))
+				return entry.getKey();
+		throw new RuntimeException("could not find parent group: "+name);
+	}
+
+	private void addBlock(String name, Vector3f position) { this.addBlock(name, position.x, position.y, position.z); }
+	private void addBlock(String name, float x, float y, float z) {
+		Mesh mesh = this.meshMap.get(name);
+		BlockItem block = new BlockItem(mesh);
+		block.setScale(BLOCK_SCALE);
+		block.setPosition(x, y, z);
+
+		String group = this.groupOf(name);
+		this.blockList.add(block);
+		this.blockMap.get(group).add(block);
+		if (this.orderMap.get(group))
+			this.reorderBlock(group);
+	}
+
+	private void reorderBlock(String group) {
+		Vector3f cameraPosition = this.camera.getPosition();
+		this.blockMap.get(group).sort(Comparator.<BlockItem>comparingDouble(
+			block -> block.getPosition().distance(cameraPosition)
+		));
+	}
+
+	private void removeBlock(BlockItem block) {
+		String name = this.nameOf(block);
+		String group = this.groupOf(name);
 		this.blockList.remove(block);
-		this.blockMap.get(mesh).remove(block);
-		if (this.blockMap.get(mesh).size() == 0)
-			this.blockMap.remove(mesh);
-		return this;
+		this.blockMap.get(group).remove(block);
 	}
 }
